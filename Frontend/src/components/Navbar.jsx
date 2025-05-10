4ff13551665df2338756c641557bf71f5904d300
Deepu <deepankarsama@gmail.com>
2025-05-10 16:04:21 +0530

    fix: ggwp

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets.js'
-import SearchBar from './searchbar'
+import SearchBar from './SearchBar'
 import '../styles/Navbar.css'
 import { useNavigate } from 'react-router-dom';
 
