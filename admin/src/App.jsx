cc39b80a4254e46633bfb7fbddf67fa2e9ca56ec
Deepu <deepankarsama@gmail.com>
2025-05-10 16:14:15 +0530

    Fix: admin impoprrt issue

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Siderbar from './components/Siderbar'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add'
