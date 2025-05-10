5a8421e95336597a33e7722007ee771e7ebadb1c
Deepu <deepankarsama@gmail.com>
2025-05-10 17:06:30 +0530

    ggwp

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react-router-dom',
-      'react-toastify',
-      'axios'
-    ]
-  }
+  server: { port: 5173}
 })
