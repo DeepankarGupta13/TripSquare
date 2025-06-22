7942b12fa66b9142ab5ffb57a198920036daab4e
Deepu <deepankarsama@gmail.com>
2025-06-23 01:57:59 +0530

    Feat: Journey in frames photos update

--- Frontend/src/components/JourneyInFrames.jsx
+++ Frontend/src/components/JourneyInFrames.jsx
@@ -1,24 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/JourneyInFrames.css';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 import { familyTripImages, privateTripImages } from '../assets/assets';
 
 const JourneyInFrames = () => {
   const photos = [
     {
-      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750097391/WhatsApp_Image_2025-06-16_at_23.37.35_1_ysbb1h.jpg',
-      alt: 'Ooty'
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621536/Black_and_White_Space_Blank_Document_12_akaaux.png',
+      alt: 'Rajasthan'
     },
     {
-      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750097391/WhatsApp_Image_2025-06-16_at_23.37.35_kwbi3d.jpg',
-      alt: 'Coorg'
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621533/Black_and_White_Space_Blank_Document_8_cdyxgr.png',
+      alt: 'Jaishalmer'
     },
     {
-      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750097390/WhatsApp_Image_2025-06-16_at_23.37.36_1_wtzr90.jpg',
-      alt: 'Kodaikanal'
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621535/Black_and_White_Space_Blank_Document_3_y7ygu1.png',
+      alt: 'Hampi'
     },
     {
       url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750097390/WhatsApp_Image_2025-06-16_at_23.37.36_pve9nr.jpg',
       alt: 'Munnar'
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621533/Black_and_White_Space_Blank_Document_likmv4.png',
+      alt: 'Kerela'
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750622168/Black_and_White_Space_Blank_Document_4_j7udec.png',
+      alt: 'Hampi',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621532/Black_and_White_Space_Blank_Document_6_tlgs0g.png',
+      alt: 'Meghalaya'
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621535/Black_and_White_Space_Blank_Document_7_czuhx4.png',
+      alt: 'Sikkim',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621531/Black_and_White_Space_Blank_Document_13_bl1dxj.png',
+      alt: 'Gokarna',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750622196/Black_and_White_Space_Blank_Document_9_xkmdsq.png',
+      alt: 'Hampi',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750622207/Black_and_White_Space_Blank_Document_14_twm9uv.png',
+      alt: 'Rajasthan',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621532/Black_and_White_Space_Blank_Document_1_rblrgh.png',
+      alt: 'Jaipur',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750621532/Black_and_White_Space_Blank_Document_2_aon9es.png',
+      alt: 'Pondicherry',
+    },
+    {
+      url: 'https://res.cloudinary.com/dfxgnf8kz/image/upload/v1750622172/Black_and_White_Space_Blank_Document_5_ofxzfv.png',
+      alt: 'Hampi',
     },
   ];
 
@@ -35,13 +76,13 @@ const JourneyInFrames = () => {
   }, [currentIndex]);
 
   const goToPrevious = () => {
-    setCurrentIndex(prev => 
+    setCurrentIndex(prev =>
       prev === 0 ? photos.length - 1 : prev - 1
     );
   };
 
   const goToNext = () => {
-    setCurrentIndex(prev => 
+    setCurrentIndex(prev =>
       prev === photos.length - 1 ? 0 : prev + 1
     );
   };
@@ -75,7 +116,7 @@ const JourneyInFrames = () => {
         <p className="slider-subtitle">Frames</p>
       </div>
 
-      <div 
+      <div
         className="slider"
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
@@ -88,21 +129,25 @@ const JourneyInFrames = () => {
             style={{ backgroundImage: `url(${photo.url})` }}
             aria-hidden={index !== currentIndex}
           >
-            <img 
-              src={photo.url} 
-              alt={photo.alt} 
+            <img
+              src={photo.url}
+              alt={photo.alt}
               className="slide-image"
               loading="lazy"
             />
+            <div className="caption">
+              <span><FaMapMarkerAlt style={{ marginRight: '6px' }} />{photo.alt}</span>
+            </div>
           </div>
         ))}
       </div>
+
       <button className="slider-button prev" onClick={goToPrevious}>
-          &#10094;
-        </button>
-        <button className="slider-button next" onClick={goToNext}>
-          &#10095;
-        </button>
+        &#10094;
+      </button>
+      <button className="slider-button next" onClick={goToNext}>
+        &#10095;
+      </button>
     </div>
   );
 };
